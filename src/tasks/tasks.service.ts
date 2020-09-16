08d81350ccb992e8ce683bbff7f81d976397a167
Nam <hoainam.301090@gmail.com>
2020-09-16 15:37:35 +0700

    Defined a model (shape) of data and applied to appropriate positions

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,10 +1,18 @@
 import { Injectable } from '@nestjs/common';
+import { Task, TaskStatus } from './task.model';
 
 @Injectable()
 export class TasksService {
-  private tasks = ['alo'];
+  private tasks: Task[] = [
+    {
+      id: '1',
+      title: 'First Item',
+      description: 'No description',
+      status: TaskStatus.DONE
+    }
+  ];
 
-  getAllTasks(): any[] {
+  getAllTasks(): Task[] {
     return this.tasks;
   }
 }
