51554d3652fc2bcf8f161b9e6ecb8544ed8c3153
Nam <hoainam.301090@gmail.com>
2020-09-24 16:27:11 +0700

    Utilized functions from base class of Repository from TypeORM
    ParseIntPipe as the second param of @Param()

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -2,8 +2,24 @@ import { Injectable, NotFoundException } from '@nestjs/common';
 import { TaskStatus } from './task-status.enum';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
+import { TaskRepository } from './task.repository';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Task } from './task.entity';
 
 @Injectable()
 export class TasksService {
- 
+  constructor(
+    @InjectRepository(TaskRepository)
+    private taskRepository: TaskRepository
+  ) {}
+
+  async getTaskById(id: number): Promise<Task> {
+    const found = await this.taskRepository.findOne(id);
+
+    if (!found) {
+      throw new NotFoundException(`Task with the id of ${id} is not found`);
+    }
+
+    return found;
+  }
 }
