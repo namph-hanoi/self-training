2310cfecc7fbe2c60c9942e32f979d56f9b10bed
Nam <hoainam.301090@gmail.com>
2020-09-25 19:33:28 +0700

    Validate Func using REGEX and custom validation message

--- src/auth/dto/auth-credentials.dto.ts
+++ src/auth/dto/auth-credentials.dto.ts
@@ -1,9 +1,14 @@
-import { IsNotEmpty } from "class-validator";
+import { IsNotEmpty, Matches, MaxLength, MinLength } from "class-validator";
 
 export class AuthCredentialsDto {
   @IsNotEmpty()
+  @MinLength(4)
+  @MaxLength(20)
   username: string;
 
   @IsNotEmpty()
+  @MinLength(4)
+  @MaxLength(20)
+  @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, { message: 'Password is too weak'})
   password: string;
 }
