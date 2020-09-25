dd74e285502ff1736fd3b2e83f18ca779bde0ea4
Nam <hoainam.301090@gmail.com>
2020-09-25 19:01:32 +0700

    [Bug] caused by CLI --> auto created AuthController in app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,10 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeOrmConfig } from './config/typeorm.config';
 import { TasksModule } from './tasks/tasks.module';
+import { AuthController } from './auth/auth.controller';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [TasksModule, TypeOrmModule.forRoot(typeOrmConfig)],
+  imports: [TypeOrmModule.forRoot(typeOrmConfig), AuthModule, TasksModule],
 })
 export class AppModule {}
