99ffb61f1cde90c6e75e82d087a9994c46448e5a
Nam <hoainam.301090@gmail.com>
2020-10-07 13:24:42 +0700

    Synchronize the config with the config files

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -1,12 +1,15 @@
 import {  TypeOrmModuleOptions } from "@nestjs/typeorm";
+import * as config from 'config';
+
+const dbConfig = config.get('db');
 
 export const typeOrmConfig: TypeOrmModuleOptions = {
-  type: 'postgres',
-  host: '103.56.156.26',
-  port: 5432,
-  username: 'postgres',
-  password: 'mysecretpassword',
-  database: 'taskmanagement',
+  type: dbConfig.type,
+  host: dbConfig.host,
+  port: dbConfig.port,
+  username: dbConfig.username,
+  password: dbConfig.password,
+  database: dbConfig.database,
   entities: [__dirname + '/../**/*.entity.{js,ts}'],
   synchronize: true
 }
