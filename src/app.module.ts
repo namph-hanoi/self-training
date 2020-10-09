e5adad6002ea9a6e020bbff2fbee11c0a24a03a4
Nam.Phan <hoainam.301090@gmail.com>
2020-10-09 20:59:24 +0700

    Added TypeORM. Created TypeORM entity. Connected to the remote DB

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 import { MongooseModule } from '@nestjs/mongoose';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CatsModule } from './cats/cats.module';
+import { Lesson } from './lesson/lesson.entity';
 import { LessonModule } from './lesson/lesson.module';
 
 @Module({
@@ -11,6 +13,15 @@ import { LessonModule } from './lesson/lesson.module';
     CatsModule,
     GraphQLModule.forRoot({
       autoSchemaFile: 'schema.gql',
+    }),
+    TypeOrmModule.forRoot({
+      type: 'mongodb',
+      url: 'mongodb://103.56.156.26/school',
+      synchronize: true,
+      useUnifiedTopology: true,
+      entities: [
+        Lesson
+      ]
     }),
     MongooseModule.forRoot('mongodb://localhost/nest'),
     LessonModule,
