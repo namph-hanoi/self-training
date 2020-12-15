ae65730a9c10c59b360cdb2e8bc3b5126e63c1a0
Nam <hoainam.301090@gmail.com>
2020-12-15 13:57:30 +0700

    Build basic configuration. NOTICE - CMD: webpack serve --config=abc/xyz

---
+++ config/webpack.dev.js
@@ -0,0 +1,17 @@
+const path = require("path");
+
+module.exports = {
+  entry: {
+    mainAlo: "./src/main.js"
+  },
+  mode: "development",
+  output: {
+    filename: "[name]-bundle.js",
+    path: path.resolve(__dirname, "../dist"),
+    publicPath: "/"
+  },
+  devServer: {
+    contentBase: "dist",
+    publicPath: "/"
+  }
+};
