71a023dbf11dd195d6c4d4aa4011af3651d88a93
Nam.Phan <hoainam.301090@gmail.com>
2021-02-10 13:34:20 +0700

    🐛 FIXED - updated transform-runtime plugin settings according to official docs

--- src/main.js
+++ src/main.js
@@ -2,6 +2,12 @@ require("babel-runtime/regenerator")
 require("./main.css");
 require("./index.html");
 
-var a = () => {
-  console.log("Hello from Nam Phan");
-}
+var example = async (args) => {
+  const { a, b } = args;
+  await console.log("Hello from Nam Phan" + a + ' ' + b);
+};
+
+example({
+  a: 'April',
+  b: '2021'
+});
