1b2486c03f3846b4c1ffc0b96674c367d1232259
Nam.Phan <hoainam.301090@gmail.com>
2021-02-28 13:50:26 +0700

    🛠 - Wrapped app with an ApolloServer 👌 - GraphQL Client runs OK

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,6 +2,7 @@ import "reflect-metadata";
 // import {createConnection} from "typeorm";
 // import {User} from "./entity/User";
 import express from 'express';
+import { ApolloServer } from "apollo-server-express";
 
 
 (() => {
@@ -9,6 +10,22 @@ import express from 'express';
 	app.get("/", (_req, res) => {
 		res.send('hello');
 	});
+
+	// APOLLO SERVER setup
+	const apolloServer = new ApolloServer({
+		typeDefs: `
+			type Query {
+				hello: String!
+			}
+		`,
+		resolvers: {
+			Query: {
+				hello: () => 'Hello bro!'
+			}
+		}
+	});
+	apolloServer.applyMiddleware({ app })
+	// \\ APOLLO SERVER setup
 
 	app.listen(4000, () => {
 		console.log("Express listen");
