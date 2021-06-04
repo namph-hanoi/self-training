51b5db816f4426b1d789379fc81a15ebdbe905d8
Nam.Phan <hoainam.301090@gmail.com>
2021-06-04 19:21:20 +0700

    🛠 ADDED cookie-parser

--- server/src/index.ts
+++ server/src/index.ts
@@ -5,12 +5,17 @@ import { ApolloServer } from "apollo-server-express";
 import { buildSchema } from 'type-graphql';
 import { UserResolver } from "./resolver/UserResolver";
 import { createConnection } from "typeorm";
-
+import cookieParser from "cookie-parser";
 
 (async () => {
 	const app = express();
+	app.use(cookieParser());
 	app.get("/", (_req, res) => {
 		res.send('hello');
+	});
+
+	app.post("/refresh_token", req => {
+		console.log(["🚀 ~ file: index.ts ~ line 17 ~ req", req.cookies]);
 	});
 
 	await createConnection();
